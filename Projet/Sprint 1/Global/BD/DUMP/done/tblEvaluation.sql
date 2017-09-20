7f6e6b37c15fede7464c1f7cdefac1fd86f86cb1
DESKTOP-BLB6I6N\marca <marcantoine_duc@hotmail.com>
2017-09-20 16:39:00 -0400

    base de données staable+Vues+insertions

---
+++ Projet/Sprint 1/Global/BD/DUMP/done/tblEvaluation.sql
@@ -0,0 +1,8 @@
+/*
+-- Query: SELECT * FROM tblEvaluation
+LIMIT 0, 1000
+
+-- Date: 2017-09-18 22:28
+*/
+INSERT INTO `tblEvaluation` (`Id`,`Titre`,`tag`) VALUES (1,'Évaluation 1',NULL);
+INSERT INTO `tblEvaluation` (`Id`,`Titre`,`tag`) VALUES (2,'Évaluation 2',NULL);
