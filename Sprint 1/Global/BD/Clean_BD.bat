d305a65e1f17789c8f05223ce33a18b32aededbf
DESKTOP-BLB6I6N\marca <marcantoine_duc@hotmail.com>
2017-09-29 13:25:51 -0400

    GAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAa

--- Sprint 1/Global/BD/Clean_BD.bat
+++ Sprint 1/Global/BD/Clean_BD.bat
@@ -1,2 +1,13 @@
-echo bonjour
-pause
+echo bonjour, je te souhaite une boonne journée.
+echo USE cegepjon_p2017_2_dev; > temp.sql
+TYPE 1.sql >> temp.sql
+TYPE DUMP\allTables.sql >> temp.sql
+echo USE cegepjon_p2017_2_tests; >> temp.sql
+TYPE 1.sql >> temp.sql
+TYPE DUMP\allTables.sql >> temp.sql
+echo USE cegepjon_p2017_2_prod; >> temp.sql
+TYPE 1.sql >> temp.sql
+TYPE DUMP\allTables.sql >> temp.sql
+mysql -h dicj.info -u cegepjon_p2017_2 -p madfpfadshdb
+PAUSE
+del temp.sql
