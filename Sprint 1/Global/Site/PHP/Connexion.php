71a7d8090af0dcded7cf894f839d192705e40a2d
David Sheehy <kolipolu@gmail.com>
2017-10-19 09:28:11 -0400

    Remise a niveau de l'ancienne version

--- Sprint 1/Global/Site/PHP/Connexion.php
+++ Sprint 1/Global/Site/PHP/Connexion.php
@@ -1,10 +1,14 @@
 <?php
+	session_start();
+	include 'ConnexionBD.php';
+	include 'hash.php';
 	$username = $_POST['Username'];
 	$MDP = $_POST['Password'];
-
+	
 	try
 	{
 		include 'Recherche.php';
 	}
 	catch(Exception $e){echo "HO NO " . $e;}
+	
 ?>
--- Sprint 1/Global/Site/PHP/Recherche.php
+++ Sprint 1/Global/Site/PHP/Recherche.php
@@ -1,41 +1,61 @@
 <?php //recherche de connexion dans la bd
-	session_start();
-	include 'connexionBD.php';
-    
-	$query = $bdd->prepare("SELECT * FROM vStagiaire WHERE CourrielScolaire = :username");
-    $query->execute(array('username'=>$username));
-	$result = $query->fetchAll();
 
-	if($result != null)
+	$query = $bdd->prepare("SELECT * FROM vUtilisateur WHERE Courriel = :username");
+	$query->execute(array('username'=>$username));
+	$connecte = $query->fetchAll();
+
+	//Vérif mdp ajout d'un if LALA
+
+	foreach ($connecte as $user) 
 	{
-		foreach($result as $stagiaire)
+		$_SESSION['idConnecte'] = $user['Id'];
+	}
+
+	//switch($_SESSION[''])
+	if (Login($username, $MDP, $bdd))
+	{
+		switch ($_SESSION['IdRole'])
 		{
-			$_SESSION['PrenomConnecte'] = $stagiaire['Prenom'];
-			$_SESSION['NomConnecte'] = $stagiaire['Nom'];
-			$_SESSION['idConnecte'] = $stagiaire['Id'];
-			$_SESSION['RoleConnecte'] = "Stagiaire";
-			include'TableauBordStagiaire.php';
+			case 1:
+			//call page using header("Location: path");
+			echo "I am a teacher";
+			break;
+
+			case 2:
+			header("Location: TBEntreprise.php");
+			break;
+
+			case 3:
+			//call page using header("Location: path");
+			echo "I am a teacher";
+			break;
+
+			case 4:
+			header("Location: TBEntreprise.php");
+			//call page using header("Location: path");
+			break;
+
+			case 5:
+					$query = $bdd->prepare("SELECT * FROM vStagiaire WHERE IdUtilisateur = :id");
+					header("Location: TableauBordStagiaire.php");
+
+			//call page using header("Location: path");
+			break;
+
+			default: echo "error unknown IdRole";
 		}
+
+		$query->execute(array('id'=>$_SESSION['idConnecte']));
+        $connecte = $query->fetchAll();
+
+        foreach ($connecte as $user)
+        {
+           	$_SESSION['PrenomConnecte'] = $user['Prenom'];
+           	$_SESSION['NomConnecte'] = $user['Nom'];
+        }
 	}
 	else
 	{
-		$query = "SELECT * FROM vEmployeEntreprise WHERE CourrielEntreprise = '$username'";
-		$result = $bdd->query($query);
-
-		if($result->num_rows > 0)
-		{
-			while($row = $result->fetch_assoc())
-			{
-				$_SESSION['PrenomConnecte'] = $row['Prenom'];
-				$_SESSION['NomConnecte'] = $row['Nom'];
-				$_SESSION['idConnecte'] = $row['Id'];
-				$_SESSION['RoleConnecte'] = "Entreprise";
-				include 'TBEntreprise.php';
-			}
-		}
-		else
-		{
-			echo 'Connexion incorrect!';
-		}
-	} //Le mot de passe devra etre verifier a quelque part dsans ce fichier pour etre sur que la personne ce doit bien connecter
+		header("Location: /equipe2Projet/Sprint%201/Global/Site/");
+	}
 ?>
