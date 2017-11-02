b8c8562c0dc883a27fc8bf5399584f244391ad05
Francis <coolfran6@hotmail.com>
2017-11-02 15:14:40 -0400

    Merge Evaluation

--- Sprint 2/Global/Site/PHP/Evaluation.php
+++ Sprint 2/Global/Site/PHP/Evaluation.php
@@ -1,5 +1,12 @@
 <?php
     
+    $query = $bdd->prepare( 'SELECT * FROM vIdentification
+                            WHERE IdStagiaire = :idStagiaire');
+
+    $query->execute(array('idStagiaire'=>$_REQUEST["idStagiaire"]));
+
+    $identification = $query->fetchAll();
+
     $content =
     '<article class="stagiaire">
         <div class="infoStagiaire">
@@ -13,7 +20,6 @@
                 se fasse conjointement avec l’élève stagiaire et que la démarche s\'effectue de
                 façon formative. Une fois complété, le formulaire devra être remis à votre
                 stagiaire qui se chargera de nous l\'expédier.
-
             </p>
         </div>
 
@@ -25,22 +31,22 @@
             <tbody>
                 <tr>
                     <td>Organisation</td>
-                    <td>Cegep de jonquière</td>
+                    <td>'.$identification[0]["NomEnt"].'</td>
                 </tr>
 
                 <tr>
                     <td>Responsable technique</td>
-                    <td>Lucien Lachance</td>
+                    <td>'.$_SESSION['PrenomConnecte'].' '.$_SESSION['NomConnecte'].'</td>
                 </tr>
 
                 <tr>
                     <td>Responsable pédagogique</td>
-                    <td>Martin Luther</td>
+                    <td>'.$identification[0]["PrenomEns"].' '.$identification[0]["NomEns"].'</td>
                 </tr>
 
                 <tr>
                     <td>Élève stagiaire</td>
-                    <td>Martin Mystere</td>
+                    <td>'.$identification[0]["PrenomSta"].' '.$identification[0]["NomSta"].'</td>
                 </tr>
             </tbody>
         </table>
@@ -52,56 +58,58 @@
                 et la persévérance.
             </p>
         </div>
+        
+        <div class="categories">
+            <table class="evaluation">
+                <thead>
+                    <th>Critères</th>
+                    <th>Généralement</th>
+                    <th>Souvent</th>
+                    <th>Parfois</th>
+                    <th>Rarement</th>
+                </thead>
+                <tbody>
+                    <tr>
+                        <td>Faire preuve de curiosité.</td>
+                        <td><input type="radio" name="Q1"/></td>
+                        <td><input type="radio" name="Q1"/></td>
+                        <td><input type="radio" name="Q1"/></td>
+                        <td><input type="radio" name="Q1"/></td>
+                    </tr>
 
-        <table class="evaluation">
-            <thead>
-                <th>Critères</th>
-                <th>Généralement</th>
-                <th>Souvent</th>
-                <th>Parfois</th>
-                <th>Rarement</th>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Faire preuve de curiosité.</td>
-                    <td><input type="radio" name="Q1"/></td>
-                    <td><input type="radio" name="Q1"/></td>
-                    <td><input type="radio" name="Q1"/></td>
-                    <td><input type="radio" name="Q1"/></td>
-                </tr>
+                    <tr>
+                        <td>
+                            Concentrer ses efforts et investir l’énergie nécessaire pour faire
+                            du bon travail, voir à se dépasser, et pour satisfaire aux normes
+                            de qualité et de rendement en vigueur dans l’entreprise.
+                        </td>
+                        <td><input type="radio" name="Q2"/></td>
+                        <td><input type="radio" name="Q2"/></td>
+                        <td><input type="radio" name="Q2"/></td>
+                        <td><input type="radio" name="Q2"/></td>
+                    </tr>
 
-                <tr>
-                    <td>
-                        Concentrer ses efforts et investir l’énergie nécessaire pour faire
-                        du bon travail, voir à se dépasser, et pour satisfaire aux normes
-                        de qualité et de rendement en vigueur dans l’entreprise.
-                    </td>
-                    <td><input type="radio" name="Q2"/></td>
-                    <td><input type="radio" name="Q2"/></td>
-                    <td><input type="radio" name="Q2"/></td>
-                    <td><input type="radio" name="Q2"/></td>
-                </tr>
+                    <tr>
+                        <td>
+                            Porter un intérêt soutenu au travail. Faire preuve de
+                            persévérance et de volonté.
+                        </td>
+                        <td><input type="radio" name="Q3"/></td>
+                        <td><input type="radio" name="Q3"/></td>
+                        <td><input type="radio" name="Q3"/></td>
+                        <td><input type="radio" name="Q3"/></td>
+                    </tr>
 
-                <tr>
-                    <td>
-                        Porter un intérêt soutenu au travail. Faire preuve de
-                        persévérance et de volonté.
-                    </td>
-                    <td><input type="radio" name="Q3"/></td>
-                    <td><input type="radio" name="Q3"/></td>
-                    <td><input type="radio" name="Q3"/></td>
-                    <td><input type="radio" name="Q3"/></td>
-                </tr>
-
-                <tr>
-                    <td>Être de bonne humeur. Éprouver du plaisir à travailler</td>
-                    <td><input type="radio" name="Q4"/></td>
-                    <td><input type="radio" name="Q4"/></td>
-                    <td><input type="radio" name="Q4"/></td>
-                    <td><input type="radio" name="Q4"/></td>
-                </tr>
-            </tbody>
-        </table>
+                    <tr>
+                        <td>Être de bonne humeur. Éprouver du plaisir à travailler</td>
+                        <td><input type="radio" name="Q4"/></td>
+                        <td><input type="radio" name="Q4"/></td>
+                        <td><input type="radio" name="Q4"/></td>
+                        <td><input type="radio" name="Q4"/></td>
+                    </tr>
+                </tbody>
+            </table>
+        </div>
 
         <div class="navigateurEval">
             <input class="bouton" style="width : 150px; float: left;" type="button" value="Précédent"/>
