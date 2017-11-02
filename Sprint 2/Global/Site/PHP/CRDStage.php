b2a834fffa8c3cfa2d3b3bba14747fe2da355785
Unknown <DarHa1531212@etu.cegepjonquiere.ca>
2017-11-02 18:31:53 -0400

    création stage

---
+++ Sprint 2/Global/Site/PHP/CRDStage.php
@@ -0,0 +1,39 @@
+<?php
+/****************************************************************************************************************
+*	Nom: Hans Darmstadt-Bélanger																				*
+*	Date: 01 Novembre 2017																						*
+*	But: recevoir les données de création ou de modification d'un stage via un post et les entrer dans la BD 	*
+*****************************************************************************************************************/
+	include 'Session.php';
+	include 'ConnexionBD.php';
+
+	/* Données à recevoir pour la création d'un stage: 
+	idResponsable, idSuperviseur, idStagiaire, idEnseignant, 
+	descriptionStage, competencesRecherche, horaireTravail, nbreHeuresSemaine, 
+	Remunere, salaireHoraire, dateDebut, dateFin*/
+function ajouterStage()
+{
+
+
+}
+
+
+
+	$idResponsable = $_POST['idResponsable'];
+	$idSuperviseur = $_POST['idSuperviseur'];
+	$idStagiaire = $_POST['idStagiaire'];
+	$idEnseignant = $_POST['idEnseignant'];
+	$descriptionStage = $_POST['descriptionStage'];
+	$competencesRecherche = $_POST['competencesRecherche'];
+	$horaireTravail = $_SESSION['horaireTravail'];
+	$nbreHeuresSemaine = $_SESSION['nbreHeuresSemaine'];
+	$salaireHoraire = $_SESSION['salaireHoraire'];
+	$dateDebut = $_SESSION['dateDebut'];
+	$dateFin = $_SESSION['dateFin'];
+
+
+	$query = $bdd->prepare("INSERT INTO tblStage (idResponsable, idSuperviseur, idStagiaire, idEnseignant, descriptionStage, competencesRecherche, horaireTravail, nbreHeuresSemaine, salaireHoraire, dateDebut, dateFin ) VALUES ('$idResponsable' , '$idSuperviseur' , '$idStagiaire', '$idEnseignant', '$descriptionStage', '$competencesRecherche', '$horaireTravail', '$nbreHeuresSemaine' , '$salaireHoraire', '$dateDebut', '$dateFin');");
+    $query->execute();
+
+
+?>
