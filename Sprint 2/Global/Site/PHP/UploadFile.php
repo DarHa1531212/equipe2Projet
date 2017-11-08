d2bef5bd28b794d842a0b4e687cd4faf098fc544
David Sheehy <kolipolu@gmail.com>
2017-11-08 15:47:21 -0500

    Petit refactoring sur le journal de bord et la regex

--- Sprint 2/Global/Site/PHP/UploadFile.php
+++ Sprint 2/Global/Site/PHP/UploadFile.php
@@ -8,24 +8,28 @@
 		$taille = filesize($_FILES['file']['tmp_name']);
 		$extensions = array('.png','.jpg','.jpeg','.docx','.pdf');
 		$extension = strchr($_FILES['file']['name'],'.');
+		$erreur = false;
 
 
 
 		if(!in_array($extension, $extensions))
 		{
-			//Ne prend pas ce type de fichier
+			?><script>alert("Ce type de fichier n'est pas valide...");</script>><?php
+			$erreur = true;
 		}
 
 		if($taille > $tailleMax)
 		{
-			//Le fichier est trop volumineux...
+			?><script>alert("Le fichier est trop volumineux...");</script>><?php
+			$erreur = true;
+
 		}
 
 		if($fichier != "")
 		{
 			$verif = verificationDoublon($bdd, $fichier);
 
-			if(!isset($erreur) && $verif) //S'il n'y a pas d'erreur
+			if($erreur && $verif) //S'il n'y a pas d'erreur
 			{
 				$fichier = strtr($fichier, 'ÀÁÂÃÄÅÇÈÉÊËÌÍÎÏÒÓÔÕÖÙÚÛÜÝàáâãäåçèéêëìíîïðòóôõöùúûüýÿ', 'AAAAAACEEEEIIIIOOOOOUUUUYaaaaaaceeeeiiiioooooouuuuyy');
 				$fichier = preg_replace('/([^.a-z0-9]+)/i','-', $fichier);
