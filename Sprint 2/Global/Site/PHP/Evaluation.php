6d088ecc49c614a6c04ef21f10b8f5c1772f9d60
Francis-PC\Francis <billotlogs@gmail.com>
2017-11-13 07:13:56 -0500

    evaluation en objet

--- Sprint 2/Global/Site/PHP/Evaluation.php
+++ Sprint 2/Global/Site/PHP/Evaluation.php
@@ -2,6 +2,11 @@
 
     $eval = new Evaluation($bdd, $_REQUEST["idEvaluation"]);
 
+    if($eval->getIdTypeEval() == 1)
+        $eval = new EvaluationGrille($bdd, $_REQUEST["idEvaluation"]);
+    else if($eval->getIdTypeEval() == 2)
+        $eval = new EvaluationChoixReponse($bdd, $_REQUEST["idEvaluation"]);
+
     function Identification($bdd){
         $query = $bdd->prepare( 'SELECT * FROM vIdentification
                             WHERE IdStagiaire = :idStagiaire');
@@ -38,104 +43,6 @@
         ';
     }
 
-    function QuestionsGrille($bdd, $eval){
-        
-        $content = "";
-        
-        foreach($eval->getCategories() as $categorie){
-            $queryQuestion->execute(array("idEvaluation"=>$_REQUEST["idEvaluation"], "idCategorie"=>$categorie["IdCategorie"]));
-            $questions = $queryQuestion->fetchAll();
-            
-            $content = $content.
-                '
-                <div class="categories">
-                    <div class="separateur" id="question">
-                        <h3>'.$categorie["Lettre"].'. '.$categorie["TitreCategorie"].'</h3>
-                        <p> 
-                            '.$categorie["descriptionCategorie"].'
-                        </p>
-                    </div>
-
-                    <table class="evaluation">
-                        <thead>
-                            <th>Critères</th>
-                            <th>Généralement</th>
-                            <th>Souvent</th>
-                            <th>Parfois</th>
-                            <th>Rarement</th>
-                        </thead>
-
-                        <tbody>';
-            
-            foreach($questions as $question){
-                $content = $content.
-                '
-                            <tr>
-                                <td>'.$question["Texte"].'</td>
-                                '.ChoixReponses($bdd, $question["Id"], $queryReponseChoisie, $reponses).'
-                            </tr>
-                ';
-            }
-            
-            $content = $content.
-                '
-                        </tbody>
-                    </table>
-                </div>
-                ';
-        }
-        
-        return $content;
-    }
-
-    function QuestionChoixReponse($bdd, $eval){
-        $content = "";
-        
-        foreach($eval->getQuestions() as $question){
-            $content = $content.
-            '<div class="categories">
-                <div class="separateur" id="question">
-                    <h3>'.$question->getCategorie()->getLettre().' '.$question->getCategorie()->getTitre().'</h3>
-                    <p> 
-                        '.$question->getTexte().'
-                    </p>
-                </div>
-                <table class="evaluation2">
-                    <tbody>          
-                            '.ChoixReponses($bdd, $question->getId(), $eval).'
-                    </tbody>
-                </table>
-            </div>';                  
-        }
-        
-        return $content;
-    }
-
-    function ChoixReponses($bdd, $idQuestion, $eval){
-        $content = "";
-    
-        foreach($eval->getQuestions() as $question){
-            foreach($question->getReponses() as $reponse){
-                if($_REQUEST["typeEval"] == 1){
-                    if($reponse->getId() == $eval->getReponses()[0]->getId())
-                        $content = $content.'<td><input type="radio" id="question'.$idQuestion.'" name="question'.$idQuestion.'" value="'.$reponse->getid().'" checked = "checked" ></td>';
-                    else
-                        $content = $content.'<td><input type="radio" name="question'.$idQuestion.'" value="'.$reponse->getid().'"></td>';
-                    }
-                else if($_REQUEST["typeEval"] == 2){
-                    $content = $content. 
-                    '
-                        <tr class="itemHover" onclick="ReponseChoisie(this)">
-                            <td>'.$reponse->getTexte().'</td>
-                        </tr>
-                    ';
-                }  
-            }
-        }
-        
-        return $content;
-    }
-
     function LettreNav($bdd, $eval){
         $i = 0;
         $content = "";
@@ -148,35 +55,13 @@
         return $content;
     }
 
-    function Submit($bdd, $queryCategorie, $queryQuestion){
-        $reponses = json_decode($_POST["tabReponse"], true);
-        
-        $requeteModificationEvaluationQuestionReponse = $bdd->prepare(  'update tblEvaluationQuestionReponse SET IdReponse = :IdReponse
-                                                                        WHERE IdEvaluation = :IdEvaluation AND IdQuestion = :IdQuestion;');
-
-        $requeteModifierStatutEvaluation = $bdd->prepare('update tblEvaluation set Statut= \'3\', DateComplétée=:DateCompletee where Id=:IdEvaluation;');
-
-        $queryCategorie->execute(array('idEvaluation'=>$_REQUEST['idEvaluation']));
-
-        $requeteModifierStatutEvaluation->execute(array('IdEvaluation'=>$_REQUEST['idEvaluation'],'DateCompletee'=>date("Y-m-d")));
-
-        $categories = $queryCategorie->fetchAll();
-
-          foreach($categories as $categorie)
-          {
-              foreach($reponses as $reponse){
-                  $requeteModificationEvaluationQuestionReponse->execute(array('IdEvaluation'=>$_REQUEST['idEvaluation'],'IdQuestion'=>$reponse["idQuestion"],'IdReponse'=>$reponse["value"]));
-              }         
-          }
-    }
-
     if(isset($_REQUEST["post"]))
-        Submit($bdd, $queryCategorie, $queryQuestion);
+        $eval->Submit($bdd);
 
     $content =
     '<article class="stagiaire">
         <div class="infoStagiaire">
-            <h2>Évaluation de mi-stage</h2>
+            <h2>'.$eval->getTitre().'</h2>
         </div>
 
         <div class="blocInfo infoProfil">
@@ -193,20 +78,8 @@
             <h3>Identification</h3>
         </div>
 
-        '.Identification($bdd).'';
-
-        if($_REQUEST["typeEval"] == 1){
-            $content = $content.
-            '
-                '.QuestionsGrille($bdd, $queryQuestion, $queryReponse, $queryReponseChoisie, $queryCategorie).'
-            ';
-        }
-        else if($_REQUEST["typeEval"] == 2){
-            $content = $content.
-            '
-                '.QuestionChoixReponse($bdd, $eval).'
-            ';
-        }
+        '.Identification($bdd).'
+        '.$eval->DrawEvaluation($bdd).'';
         
         $content = $content .
         '<div class="navigateurEval">
