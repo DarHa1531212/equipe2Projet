356d142394150ca3711822bd8402d74a9d643ea1
David Sheehy <kolipolu@gmail.com>
2017-11-15 15:44:03 -0500

    okidou

--- Sprint 2/Global/Site/PHP/SessionTimeout.php
+++ Sprint 2/Global/Site/PHP/SessionTimeout.php
@@ -8,7 +8,7 @@
 		*********************************************************************/
 		function verifyTimeout()
 		{
-				if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 5))
+				if (isset($_SESSION['LAST_ACTIVITY']) && (time() - $_SESSION['LAST_ACTIVITY'] > 600))
 				{
 					session_unset();     // unset $_SESSION variable for the run-time 
 					session_destroy();   // destroy session data in storage
