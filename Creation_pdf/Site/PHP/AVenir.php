40f8f274ba05c2ecf79076cb6ad20f5af9f30bb5
Hans Bélanger <DarHa1531212@etu.cegepjonquiere.ca>
2017-12-12 16:43:06 -0500

    pop stash

--- Creation_pdf/Site/PHP/AVenir.php
+++
@@ -1,4 +0,0 @@
-<?php
-    $content = "";
-    return $content;
-?>
--- Creation_pdf/Site/js/scripts.js
+++
@@ -1,6 +0,0 @@
-function ReponseChoisie(item){
-    $(".evaluation2 > tbody > tr").css("background-color", "rgba(0, 0, 0, 0.2)");
-    $(".evaluation2 > tbody > tr:hover").css("background-color", "rgba(0, 0, 0, 0.9)");
-    
-    $(item).css("background-color", "#43ee2d");
-}
