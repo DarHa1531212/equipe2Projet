bef409497100195015a3c50be2540d40527d36e4
CEGEPJONQUIERE\AtoFr1532089 <atofr1532089@etu.cegepjonquiere.ca>
2017-12-14 12:21:14 -0500

    bug affichage stagiaire sans stage réglé.

--- Site/PHP/Profil.php
+++ Site/PHP/Profil.php
@@ -10,7 +10,8 @@
                                 ON R.Id = UR.IdRole
                                 WHERE IdUtilisateur = :id", array("id"=>$_REQUEST["id"]), "stdClass")[0];
         
-        if($role->IdRole != 5){
+        if($role->IdRole != 5)
+        {
             $profil = $bdd->Request("   SELECT Emp.IdUtilisateur, Prenom, Emp.Nom, IdRole,
                                         Ent.Nom AS 'NomEntreprise', Emp.CourrielEntreprise, Emp.NumTelEntreprise, Poste
                                         FROM vEmploye AS Emp
@@ -22,8 +23,28 @@
                                         array("id"=>$_REQUEST["id"]),
                                         "ProfilEmploye")[0];
         }
-        else{
-            $profil = $bdd->Request("   SELECT Stagiaire.IdUtilisateur, Stagiaire.Prenom, Stagiaire.Nom, Stagiaire.NumTelPerso, Stagiaire.CourrielPersonnel, Stagiaire.CodePermanent,
+        else{//stagiaire
+            
+
+            $stagiaire = $bdd->Request("  select *
+                                            from vstage as stage
+                                            where stage.IdStagiaire = :IdStagiaire",
+                                        array("IdStagiaire"=>$_REQUEST["id"]),
+                                        "stdClass");
+            if(count($stagiaire)==0)//le stagiaire n'a pas de stage
+            {
+                 $profil = $bdd->Request("SELECT Stagiaire.IdUtilisateur, Stagiaire.Prenom, Stagiaire.Nom, Stagiaire.NumTelPerso, Stagiaire.CourrielPersonnel, Stagiaire.CourrielScolaire, 
+                                        Stagiaire.CodePermanent, Stagiaire.CourrielEntreprise, Stagiaire.NumTelEntreprise, Stagiaire.Poste,'Aucun' AS 'NomEntreprise', IdRole
+                                        FROM vStagiaire AS Stagiaire
+                                        JOIN vUtilisateurRole AS UR
+                                        ON UR.IdUtilisateur = Stagiaire.IdUtilisateur
+                                        WHERE Stagiaire.IdUtilisateur = :id", 
+                                       array("id"=>$_REQUEST["id"]),
+                                        "ProfilStagiaire")[0];
+            }
+            else
+            {
+                $profil = $bdd->Request(" SELECT Stagiaire.IdUtilisateur, Stagiaire.Prenom, Stagiaire.Nom, Stagiaire.NumTelPerso, Stagiaire.CourrielPersonnel, Stagiaire.CodePermanent,
                                         Stagiaire.CourrielEntreprise, Stagiaire.NumTelEntreprise, Stagiaire.Poste, Ent.Nom AS 'NomEntreprise', IdRole
                                         FROM vStage AS Stage
                                         JOIN vStagiaire AS Stagiaire
@@ -37,6 +58,22 @@
                                         WHERE Stagiaire.IdUtilisateur = :id",
                                         array("id"=>$_REQUEST["id"]),
                                         "ProfilStagiaire")[0];
+            }
+
+            /*$profil = $bdd->Request("   SELECT Stagiaire.IdUtilisateur, Stagiaire.Prenom, Stagiaire.Nom, Stagiaire.NumTelPerso, Stagiaire.CourrielPersonnel, Stagiaire.CodePermanent,
+                                        Stagiaire.CourrielEntreprise, Stagiaire.NumTelEntreprise, Stagiaire.Poste, Ent.Nom AS 'NomEntreprise', IdRole
+                                        FROM vStage AS Stage
+                                        JOIN vStagiaire AS Stagiaire
+                                        ON Stage.Id = Stagiaire.Id
+                                        JOIN vEmploye AS Emp
+                                        ON Emp.IdUtilisateur = Stage.IdSuperviseur
+                                        JOIN vEntreprise AS Ent
+                                        ON Ent.Id = Emp.IdEntreprise
+                                        JOIN vUtilisateurRole AS UR
+                                        ON UR.IdUtilisateur = Stagiaire.IdUtilisateur
+                                        WHERE Stagiaire.IdUtilisateur = :id",
+                                        array("id"=>$_REQUEST["id"]),
+                                        "ProfilStagiaire")[0];*/
         } 
         
         $role = '('.$role->Titre.')';
