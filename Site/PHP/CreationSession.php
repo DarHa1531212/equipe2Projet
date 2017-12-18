37c9eca861518776747d5a901562f2f96d706d96
Francis <coolfran6@hotmail.com>
2017-12-18 02:50:18 -0500

    Modif session

--- Site/PHP/CreationSession.php
+++ Site/PHP/CreationSession.php
@@ -12,7 +12,7 @@ if(isset($_REQUEST["post"]))
         $bdd->Request(" INSERT INTO tblSession (Annee,Periode,MiStageDebut,MiStageLimite,FinaleDebut,FinaleLimite,FormationDebut,FormationLimite,JanvierDebut,JanvierLimite,FevrierDebut,FevrierLimite,MarsDebut,MarsLimite) 
                         VALUES (:annee,:periode,:mistagedebut,:mistagelimite,:finaledebut,:finalelimite,:formationdebut,:formationlimite,:janvierdebut,:janvierlimite,:fevrierdebut,:fevrierlimite,:marsdebut,:marslimite)",
                         array(
-                        'annee'=>$session["annee"],
+                        'annee'=>$session["dateSelected"],
                         'periode'=>$session["periode"],    
                         'mistagedebut'=>$session["mistagedebut"],    
                         'mistagelimite'=>$session["mistagelimite"],
